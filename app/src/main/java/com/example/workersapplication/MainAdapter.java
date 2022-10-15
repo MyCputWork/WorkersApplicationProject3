4112473696d1ee283c8bc6c433d8002415de3c06
Ziyaad <Zpetersen@Bainscorp.com>
2022-10-15 17:03:38 +0200

    Functionality for completing an order

--- app/src/main/java/com/example/workersapplication/MainAdapter.java
+++ app/src/main/java/com/example/workersapplication/MainAdapter.java
@@ -1,10 +1,15 @@
 package com.example.workersapplication;
 
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,7 +34,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<OrderModel, MainAdapter
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull OrderModel model) {
+    protected void onBindViewHolder(@NonNull myViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull OrderModel model) {
         holder.userEmail.setText(model.getUserEmail());
         holder.product.setText(model.getProduct().get(0));
         if (model.getProduct().size()>1){
@@ -44,6 +50,29 @@ public class MainAdapter extends FirebaseRecyclerAdapter<OrderModel, MainAdapter
                 .error(R.drawable.ic_launcher_background)
                 .into(holder.img);
 
+        holder.btnComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.userEmail.getContext());
+                builder.setTitle("Is the order completed?");
+               builder.setMessage("Completed orders cannot be undone");
+                builder.setPositiveButton("Complete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseDatabase.getInstance().getReference().child("Orders").child(getRef(position).getKey()).removeValue();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(holder.userEmail.getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                builder.show();
+            }
+        });
+
     }
 
     @NonNull
@@ -57,7 +86,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<OrderModel, MainAdapter
         CircleImageView img;
         TextView userEmail, product, product2;
 
-
+        Button btnEdit, btnComplete;
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -67,6 +96,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<OrderModel, MainAdapter
             product2 = (TextView) itemView.findViewById(R.id.productText2);
 //            product2 = (TextView) itemView.findViewById(R.id.productText2);
 
+            btnComplete = (Button) itemView.findViewById(R.id.btnComplete);
         }
     }
 
